39cc278466ff7701419c6c44e13d9d077002550b
gopikaalekhyamajeti <gopikaalekhyamajeti@gmail.com>
2026-02-02 11:17:57 +0530

    Rename resources for logging application setup

--- FullStack-Blogging-App-main/EKS_Terraform/main.tf
+++ FullStack-Blogging-App-main/EKS_Terraform/main.tf
@@ -1,56 +1,56 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "eu-north-1a"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+resource "aws_vpc" "loggingapp_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "loggingapp-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
+resource "aws_subnet" "loggingapp_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  vpc_id                  = aws_vpc.loggingapp_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.loggingapp_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["eu-north-south-1a", "eu-north-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "loggingapp-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_internet_gateway" "logging_igw" {
+  vpc_id = aws_vpc.loggingapp_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "loggingapp-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_route_table" "loggingapp_route_table" {
+  vpc_id = aws_vpc.loggingapp_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.loggingapp_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "loggingapp-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  subnet_id      = aws_subnet.loggingapp_subnet[count.index].id
+  route_table_id = aws_route_table.loggingapp_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "loggingapp_cluster_sg" {
+  vpc_id = aws_vpc.loggingapp_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "loggingapp-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "loggingapp_node_sg" {
+  vpc_id = aws_vpc.loggingapp_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "loggingapp-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+resource "aws_eks_cluster" "loggingapp" {
+  name     = "loggingapp-cluster"
+  role_arn = aws_iam_role.loggingapp_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+    subnet_ids         = aws_subnet.loggingapp_subnet[*].id
+    security_group_ids = [aws_security_group.loggingapp_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+resource "aws_eks_node_group" "loggingapp" {
+  cluster_name    = aws_eks_cluster.loggingapp.name
+  node_group_name = "loggingapp-node-group"
+  node_role_arn   = aws_iam_role.loggingapp_node_group_role.arn
+  subnet_ids      = aws_subnet.loggingapp_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -108,16 +108,16 @@ resource "aws_eks_node_group" "devopsshack" {
     min_size     = 3
   }
 
-  instance_types = ["t2.large"]
+  instance_types = ["t2.small"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+    source_security_group_ids = [aws_security_group.loggingapp_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+resource "aws_iam_role" "loggingapp_cluster_role" {
+  name = "loggingapp-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,8 +135,8 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "loggingapp_cluster_role_policy" {
+  role       = aws_iam_role.loggingapp_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
@@ -159,17 +159,17 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "loggingapp_node_group_role_policy" {
+  role       = aws_iam_role.loggingapp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "loggingapp_node_group_cni_policy" {
+  role       = aws_iam_role.loggingapp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "loggingapp_node_group_registry_policy" {
+  role       = aws_iam_role.loggingapp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
